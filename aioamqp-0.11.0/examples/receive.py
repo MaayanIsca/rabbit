7e445e00fdb7cce9f100692222781e7fe70abd78
MaayanIsca <maayan.isca@gmail.com>
2018-10-31 13:38:53 +0200

    Update receive.py

--- aioamqp-0.11.0/examples/receive.py
+++ aioamqp-0.11.0/examples/receive.py
@@ -25,45 +25,49 @@ def callback(channel, body, envelope, properties):
     type=body.decode().split(" ")[1]
     conn = sqlite3.connect(path)
     c = conn.cursor()
+    fd = open('C:\\Users\\Cameras\\Desktop\\aioamqp-0.11.0\\sql.sql', 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
     f= open('C:\\Users\\Cameras\\Desktop\\test.'+type,'w+')
     if type=='json':
-        rows =c.execute("select c.FirstName||' '||c.LastName as name, c.Phone, c.Email,c.Address||' '||c.City||' '||c.Country as adress from customers c").fetchall()
-        column = [t[0] for t in c.description]
-        for row in rows:
-            json_dict = {}
-            for j in range(len(column)):
-                key_j = column[j]  # a string depending on j
-                json_dict[key_j] = row[j]
-            f.write(json.dumps(json_dict, indent=3))
+        for command in sqlCommands:
+             rows=c.execute(command).fetchall()
+             if not c.description:
+                columns = []
+             else:
+                column = [t[0] for t in c.description]
+             for row in rows:
+                json_dict = {}
+                for j in range(len(column)):
+                    key_j = column[j]
+                    json_dict[key_j] = row[j]
+                f.write(json.dumps(json_dict, indent=3))
         conn.close()
     elif type=='xml':
-        rows =c.execute("select c.FirstName||' '||c.LastName as name, c.Phone, c.Email,c.Address||' '||c.City||' '||c.Country as adress from customers c").fetchall()
-        column = [t[0] for t in c.description]
-        for row in rows:
-            json_dict = {}
-            for j in range(len(column)):
-                key_j = column[j]  # a string depending on j
-                json_dict[key_j] = row[j]
-            # xmltools.WriteToXMLString(json.dumps(json_dict, indent=3))
-            xml=dicttoxml.dicttoxml(json_dict, attr_type=False)
-            f.write(str(xml))
-#     # xml = ''.join(row[0] for row in rows)
-    #     print(dicttoxml.dicttoxml(rows))
-    #     f.write(dicttoxml.dicttoxml(rows))
-    #     # f.write(xml)
-        conn.close()
+      for command in sqlCommands:
+             rows=c.execute(command).fetchall()
+             if not c.description:
+                columns = []
+             else:
+                column = [t[0] for t in c.description]
+             for row in rows:
+                json_dict = {}
+                for j in range(len(column)):
+                    key_j = column[j]
+                    json_dict[key_j] = row[j]
+                xml=dicttoxml.dicttoxml(json_dict, attr_type=False)
+                f.write(str(xml))
+      conn.close()
     elif type=='csv':
         workbook = Workbook('C:\\Users\\Cameras\\Desktop\\test.csv')
         worksheet = workbook.add_worksheet()
-        rows=c.execute("select c.FirstName||' '||c.LastName as name, c.Phone, c.Email,c.Address||' '||c.City||' '||c.Country as adress from customers c")
-        for i, row in enumerate(rows):
-            for j, value in enumerate(row):
-              worksheet.write(i,j,value)
+        for command in sqlCommands:
+            rows=c.execute(command)
+            for i, row in enumerate(rows):
+                for j, value in enumerate(row):
+                 worksheet.write(i,j,value)
         workbook.close()
-    #     writer = csv.writer(f, delimiter=',')
-    #     writer.writerow([ i[0] for i in c.description ])
-    #     for row in rows:
-    #         writer.writerow([i[0] for i in row ])
         conn.close()
 
     f.close()
@@ -74,9 +78,9 @@ def receive():
     transport, protocol = yield from aioamqp.connect()
     channel = yield from protocol.channel()
 
-    yield from channel.queue_declare(queue_name='a')
+    yield from channel.queue_declare(queue_name='b')
 
-    yield from channel.basic_consume(callback, queue_name='a')
+    yield from channel.basic_consume(callback, queue_name='b')
 
 
 event_loop = asyncio.get_event_loop()
